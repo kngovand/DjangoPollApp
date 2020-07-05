479f9c3b2144d6148168d56504c5615518fe84e7
K Duc <kngovand@gmail.com>
2020-07-05 15:58:06 -0600

    /polls url added. /admin working as well

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import httpResponse
+from django.http import HttpResponse
 
 def index(request):
-    return httpResponse("hi there. you are in polls index.")
+    return HttpResponse("hi there. you are in polls index.")
