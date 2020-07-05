c39d4e9bd76f42c1e89150adae1fe755a20cbcb3
K Duc <kngovand@gmail.com>
2020-07-05 16:18:24 -0600

    messed with URL routing and set time zone.

--- myapp/polls/urls.py
+++ myapp/polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('2/', views.index, name='polls2'),
+
 ]
--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hi there. you are in polls index.")
+
+def polls2(request):
+    return HttpResponse("hi there. you are in polls2 index.")
+
