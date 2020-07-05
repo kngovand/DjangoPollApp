4343674d6e6588a4678d27aeca3c0ffbcc4e3fb5
K Duc <kngovand@gmail.com>
2020-07-05 15:34:27 -0600

    polls index added

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import httpResponse
 
-# Create your views here.
+def index(request):
+    return httpResponse("hi there. you are in polls index.")
