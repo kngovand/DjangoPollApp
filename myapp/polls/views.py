27ee22d553acdf4b4ccfc206795228a734b42696
K Duc <kngovand@gmail.com>
2020-07-05 16:25:03 -0600

    included polls app

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -4,6 +4,4 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("hi there. you are in polls index.")
 
-def polls2(request):
-    return HttpResponse("hi there. you are in polls2 index.")
 
